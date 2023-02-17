d377382005093e49a3c42667ec8f729e10ee88e3
Roman <pilipop1111@gmail.com>
2023-02-17 23:13:32 +0300

    Color changed

--- hackatone_phone/lib/main_screen/main_screen_widget.dart
+++ hackatone_phone/lib/main_screen/main_screen_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../list_real_estate/list_real_estate_widget.dart';
+import '../resources/app_images.dart';
 
 class MainScreenWidget extends StatefulWidget {
   const MainScreenWidget({super.key});
@@ -10,15 +11,45 @@ class MainScreenWidget extends StatefulWidget {
 }
 
 class _MainScreenWidgetState extends State<MainScreenWidget> {
-  int _selectedTab = 0;
+  int _selectedTab = 1;
+
   void onSelectTab(int index) {
-    if (_selectedTab == index) {
-      return;
-    }
+    if (_selectedTab == index) return;
     setState(() {
       _selectedTab = index;
     });
   }
+
+  void _onEstateTap(int index) {
+    // final id = _realEstates[index].id;
+    // Navigator.of(context).pushNamed('/main_screen/estate_info', arguments: id);
+  }
+  final _myEstates = [
+    RealEstate(
+      id: 1,
+      imageName: HouseImage.image,
+      developer: 'Бауинвест',
+      district: 'Симферопольская 134',
+      description:
+          'Дом от лучших застройщиков Краснодара, красивый район и совр...',
+    ),
+    RealEstate(
+      id: 1,
+      imageName: HouseImage.image,
+      developer: 'Бауинвест',
+      district: 'Симферопольская 134',
+      description:
+          'Дом от лучших застройщиков Краснодара, красивый район и совр...',
+    ),
+    RealEstate(
+      id: 1,
+      imageName: HouseImage.image,
+      developer: 'Бауинвест',
+      district: 'Симферопольская 134',
+      description:
+          'Дом от лучших застройщиков Краснодара, красивый район и совр...',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,43 +57,243 @@ class _MainScreenWidgetState extends State<MainScreenWidget> {
       body: IndexedStack(
         index: _selectedTab,
         children: [
-          Text('Биба'),
-          Text('Boba'),
+          Container(
+            color: const Color(0xFF1A1D24),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 250,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFAFB0B2),
+                    border: Border.all(color: Colors.black.withOpacity(0.2)),
+                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 8,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Column(
+                    children: const [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Placeholder(),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'Имя пользователя',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Почта',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 35,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(37, 40, 47, 1),
+                    border: Border.all(
+                      color: const Color.fromRGBO(63, 66, 73, 1),
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 8,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Stack(
+                    children: [
+                      ListView.builder(
+                          padding: const EdgeInsets.only(top: 70),
+                          keyboardDismissBehavior:
+                              ScrollViewKeyboardDismissBehavior.onDrag,
+                          itemCount: _myEstates.length,
+                          itemExtent: 163,
+                          itemBuilder: (BuildContext context, int index) {
+                            final realEstate = _myEstates[index];
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 10),
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xFFAFB0B2),
+                                      border: Border.all(color: Colors.black),
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(10)),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.1),
+                                          offset: Offset(0, 2),
+                                          blurRadius: 8,
+                                        ),
+                                      ],
+                                    ),
+                                    clipBehavior: Clip.hardEdge,
+                                    child: Row(
+                                      children: [
+                                        Image(
+                                            image: AssetImage(
+                                                realEstate.imageName)),
+                                        const SizedBox(
+                                          width: 14,
+                                        ),
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              const SizedBox(
+                                                height: 18,
+                                              ),
+                                              Text(
+                                                realEstate.developer,
+                                                style: const TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                                maxLines: 1,
+                                              ),
+                                              const SizedBox(
+                                                height: 5,
+                                              ),
+                                              Text(
+                                                realEstate.district,
+                                                style: const TextStyle(
+                                                    color: Colors.blue),
+                                                maxLines: 1,
+                                              ),
+                                              const SizedBox(
+                                                height: 18,
+                                              ),
+                                              Text(
+                                                realEstate.description,
+                                                maxLines: 2,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          height: 10,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Material(
+                                    color: Colors.transparent,
+                                    child: InkWell(
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(10)),
+                                      onTap: () => _onEstateTap(index),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          }),
+                      Container(
+                        width: double.infinity,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFAFB0B2),
+                          border: Border.all(color: const Color(0xFFAFB0B2)),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20),
+                          ),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Недвижимость',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: const Color.fromRGBO(26, 29, 36, 1),
+            child: RealEstateListWidget(),
+          ),
         ],
       ),
       bottomNavigationBar: Container(
         decoration: const BoxDecoration(
           borderRadius: BorderRadius.only(
-              topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+            topRight: Radius.circular(30),
+            topLeft: Radius.circular(30),
+          ),
           boxShadow: [
             BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
           ],
         ),
-        child: ClipRRect(
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(30.0),
-            topRight: Radius.circular(30.0),
-          ),
-          child: BottomNavigationBar(
-            iconSize: 48,
-            currentIndex: _selectedTab,
-            items: [
-              const BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
+        child: Container(
+          color: Color(0xFF1A1D24),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0),
+            ),
+            child: BottomNavigationBar(
+              iconSize: 30,
+              currentIndex: _selectedTab,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                  ),
+                  label: '',
                 ),
-                label: '',
-              ),
-              const BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.account_circle_sharp,
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.account_circle_sharp,
+                  ),
+                  label: '',
                 ),
-                label: '',
-              ),
-            ],
-            onTap: (index) {
-              onSelectTab(index);
-            },
+              ],
+              onTap: onSelectTab,
+            ),
           ),
         ),
       ),
