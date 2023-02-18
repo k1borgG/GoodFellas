0e21c2e1719debbc2c3b062bd57f3801b0b7fed3
k1borG <kiborg66666666@yandex.ru>
2023-02-19 02:29:28 +0300

    recom

--- hack/src/components/Recomend.js
+++ hack/src/components/Recomend.js
@@ -3,50 +3,41 @@ import '../App.css';
 //import { Link } from 'react-scroll';
 import './Recomend.css';
 
-import jsonObj from '../json_data/out_1.json'
+import jsonObj from '../json_data/data2.json'
 var userObj = JSON.parse(JSON.stringify(jsonObj));
 
-console.log(userObj.apartment[0].specs[0].floors[0].apart_price)
-
 var blockList = userObj.apartment.map(key =>
-    <li> 
+    <li className='card'> 
         <div className='block'>
+            <div className='block_img'>   
+                <img src={key.apartment_info.apart_img}></img>
+            </div>
             <div className='block_JK'>   
-                Название: {key['apart_name']}
+                Название: {key.apartment_info.apart_name}
             </div>
-            <div className='block_type'>   
-                тип кв: {key.specs[0].apart_spec}
+            <div className='opisan'>
+                <div className='block_category'>   
+                    Категория: {key.category}
+                </div>
+                <div className='block_type'>   
+                    тип: {key.apartment_info.spec.apart_spec}
+                </div>
+                <div className='block_floor'>   
+                    размещение: {key.apartment_info.spec.apart_floor}
+                </div>
+                <div className='block_cost'>
+                    Цена за 1кв.м: {key.apartment_info.spec.apart_price.toLocaleString('ru-RU')}
+                </div> 
+                <div className='block_profit'>
+                    Доходность: {Math.floor(key.apartment_info.spec.apart_price/key.apartment_info.spec.apart_profit)}%
+                </div>
             </div>
-            <div className='block_floor'>   
-                этаж(и): {key.specs[0].floors[0].apart_floor}
-            </div>
-            <div className='block_cost'>
-                Цена за 1кв.м: {key.specs[0].floors[0].apart_price}
-            </div> 
-        </div>
-
+            <button className='bt_invest'>
+                <div className='tx'>Инвестировать</div>
+            </button>
+        </div>  
     </li>
   );
-// var blockList = userObj.apartment.map(key =>
-//     <li> 
-//         <div className='block'>
-//             <div className='block_JK'>   
-//                 Название: {key['apart_name']}
-//             </div>
-//             <div className='block_type'>   
-//                 тип кв: {key.specs[0].apart_spec}
-//             </div>
-//             <div className='block_floor'>   
-//                 этаж(и): {key.specs[0].floors[0].apart_floor}
-//             </div>
-//             <div className='block_cost'>
-//                 Цена за 1кв.м: {key.specs[0].floors[0].apart_price}
-//             </div> 
-//         </div>
-
-//     </li>
-//   );
-
 
 function Recomend() {
     return (
@@ -56,7 +47,7 @@ function Recomend() {
             </div>
             <div className="box_items">
                 <div className='blocks'>
-                    <ul>{blockList}</ul>
+                    <ul>{blockList.slice(130,133)}</ul>
                 </div>
             </div>
         </div>
